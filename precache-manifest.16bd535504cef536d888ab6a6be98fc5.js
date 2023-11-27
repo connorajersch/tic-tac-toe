35f5a4ed67f8b9b7b3c72827e2594b7dabb6486f
Connor Ajersch <ajersch@uwindsor.ca>
2023-11-26 20:59:32 -0500

    Updates

---
+++ precache-manifest.16bd535504cef536d888ab6a6be98fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df220aa48300a4e3b50a6a18b9a91b56",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "699702205a8f4c8807d4",
+    "url": "/tic-tac-toe/static/css/main.614ac252.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/tic-tac-toe/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "699702205a8f4c8807d4",
+    "url": "/tic-tac-toe/static/js/main.7d4a55f2.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
